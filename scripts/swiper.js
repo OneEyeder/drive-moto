db8231205138d577c29647cf7a22f7533d07b1cd
Дмитрий Новиков <120933936+OneEyeder@users.noreply.github.com>
2025-11-12 02:26:18 +0300

    Add files via upload

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,8 +1,17 @@
     document.addEventListener('DOMContentLoaded', () => {
-      const swiper = new Swiper('.swiper', {
+      const mainSwiper = new Swiper('.main-swiper', {
         speed: 400,
         spaceBetween: 100,
         pagination: { el: '.swiper-pagination', clickable: true },
         navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' }
       });
     });
+
+    const moreSwiper = new Swiper('.more-swiper', {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      // navigation: {
+      //   nextEl: '.more-next',
+      //   prevEl: '.more-prev',
+      // }
+    })
